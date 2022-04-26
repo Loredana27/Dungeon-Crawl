32a0b52529ef0af5413edebf27f2895c1fd797a7
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2022-04-27 00:28:42 +0300

    Implemented Bat as "Moving Randomly Enemy"

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Bat.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Bat.java
@@ -21,8 +21,7 @@ public class Bat extends Enemy{
 
     @Override
     public void chooseAMove(Player player) {
-        if(isPlayerInRange(player)) moveToPlayer(player);
-        else moveToPatrol();
+        moveToPatrol();
     }
 
     @Override
@@ -36,17 +35,10 @@ public class Bat extends Enemy{
 
     @Override
     public void moveToPlayer(Player player) {
-        int dx,dy;
-
-        dx = Integer.compare(player.getX() - this.getX(), 0);
-
-        dy = Integer.compare(player.getY() - this.getY(), 0);
-
-        move(dx,dy);
     }
 
     @Override
     public boolean isPlayerInRange(Player player) {
-        return (Math.abs(this.getX() - player.getX()) < 3 && Math.abs(this.getY() - player.getY()) < 3);
+        return false;
     }
 }
